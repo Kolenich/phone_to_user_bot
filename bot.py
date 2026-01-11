7ece2012a27968706ce6e773b8a3f89cfdde3c52
Kolenich <zhigalin.nick@yandex.ru>
2026-01-11 16:19:22 +0300

    Правки в код

--- bot.py
+++ bot.py
@@ -1,10 +1,23 @@
-import re
+import logging
 import os
+import re
+import sys
 
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, filters, MessageHandler
 
+load_dotenv()
+
 TOKEN = os.getenv('TOKEN')
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
 
 def normalize_phone_number(text: str) -> str | None:
@@ -24,21 +37,29 @@ def normalize_phone_number(text: str) -> str | None:
     return None
 
 
-async def handle_message(update: Update, *args, **kwargs):
-    phone_number = normalize_phone_number(update.message.text)
+async def handle_message(update: Update, context):
+    phone_number = update.message.text
 
-    if not phone_number:
+    logger.info(f'{phone_number=}')
+
+    normalized_phone_number = normalize_phone_number(phone_number)
+
+    if not normalized_phone_number:
         await update.message.reply_text(
             'Пожалуйста, введите российский номер телефона'
         )
     else:
-        link = f'https://t.me/{phone_number}'
+        link = f'https://t.me/{normalized_phone_number}'
 
         await update.message.reply_text(link)
 
 
-if __name__ == '__main__':
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
